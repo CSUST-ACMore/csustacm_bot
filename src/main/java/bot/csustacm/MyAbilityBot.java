5d7c28957cb6de2f7636c0bd16d6af776428f911
QAQorz <1059037957@qq.com>
2019-03-19 18:50:27 +0800

    update

--- src/main/java/bot/csustacm/MyAbilityBot.java
+++ src/main/java/bot/csustacm/MyAbilityBot.java
@@ -8,6 +8,8 @@ import static org.telegram.abilitybots.api.objects.Locality.*;
 import static org.telegram.abilitybots.api.objects.Privacy.*;
 
 public class MyAbilityBot extends AbilityBot {
+    private boolean defaultSwitchOpen = false;
+    private boolean recordSwitchOpen = false;
 
     MyAbilityBot() {
         super(Config.TOKEN, Config.BOT_NAME);
@@ -38,14 +40,48 @@ public class MyAbilityBot extends AbilityBot {
                 .build();
     }
 
+    public Ability sayZhengDaLaoTianXiaDiYi(){
+        return Ability.builder()
+                .name("zdltxdy")
+                .info("tqlwsl")
+                .locality(ALL)
+                .privacy(PUBLIC)
+                .action(ctx -> silent.send("政大佬天下第一！", ctx.chatId()))
+                .build();
+    }
+
+//    public Ability recordNetFriendMessage(){
+//        return Ability.builder()
+//                .name("record")
+//                .info("复读机！")
+//                .locality(ALL)
+//                .privacy(PUBLIC)
+//                .action(ctx -> silent.send())
+//    }
+
+    public Ability openBot(){
+        return Ability.builder()
+                .name("bot")
+                .info("Gwolf9！")
+                .locality(GROUP)
+                .privacy(PUBLIC)
+                .action(ctx -> {
+                    defaultSwitchOpen = !defaultSwitchOpen;
+                    silent.send(defaultSwitchOpen ? "Gwolf9天下第一!" : "溜了嘤嘤嘤", ctx.chatId());
+                })
+                .build();
+    }
+
     public Ability defalut() {
         return Ability.builder()
                 .name(DEFAULT)
                 .flag(MESSAGE)
                 .privacy(PUBLIC)
                 .locality(ALL)
-                .action(ctx -> silent.send("沙雕网友嗦的什么几把", ctx.chatId()))
+                .action(ctx -> {
+                    if (!defaultSwitchOpen) return;
+                    silent.send("沙雕网友嗦的什么几把", ctx.chatId());
+                })
                 .build();
     }
-
 }
